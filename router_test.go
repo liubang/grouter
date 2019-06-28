7efb44ebc63bfba2f9b30dd8bd6e69110e8d7364
liubang <it.liubang@gmail.com>
2019-06-28 14:12:28 +0800

    增加单元测试

--- router_test.go
+++ router_test.go
@@ -10,8 +10,18 @@ func TestRouter_Lookup(t *testing.T) {
 	router.Get("/aaa/@name:([a-z]+)/@age:([0-9]+)", nil)
 	router.Put("/bbb/@age:([0-9]+)", nil)
 	router.Get("/ccc/@name:([a-z]+)/@age:([0-9]+)", nil)
+	router.Delete("/this/is/test", nil)
 	rule, found := router.Lookup("GET", "/ccc/liubang/26")
-	if found {
+	if !found {
+		t.Error("查找路由失败")
+	} else {
+		fmt.Println(rule)
+	}
+
+	rule, found = router.Lookup("DELETE", "/this/is/test")
+	if !found {
+		t.Error("查找路由失败")
+	} else {
 		fmt.Println(rule)
 	}
 }
